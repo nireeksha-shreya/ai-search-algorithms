8ffec99198d8fe5dc4fc4d4c345279a0ffae8a85
Nireeksha Alva <shreyanireeksha@gmail.com>
2026-02-20 18:43:21 +0530

    dfs.py

---
+++ dfs.py
@@ -0,0 +1,19 @@
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(graph, neighbor, visited)
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D'],
+    'C': ['E'],
+    'D': [],
+    'E': []
+}
+
+dfs(graph, 'A')
