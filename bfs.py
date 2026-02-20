abdf1d434bd74a5949a048a9ac0448e13536df2d
Nireeksha Alva <shreyanireeksha@gmail.com>
2026-02-20 18:42:33 +0530

    bfs.py

---
+++ bfs.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            queue.extend(graph[node])
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D'],
+    'C': ['E'],
+    'D': [],
+    'E': []
+}
+
+bfs(graph, 'A')
